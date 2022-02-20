e37f5d398f35ab6bd96625d5b0f727dd53351472
Anderson Sanchez <andersonsanchez53@gmail.com>
2022-02-19 19:04:00 -0700

    add comment

--- js/global.js
+++ js/global.js
@@ -4,6 +4,7 @@ $(() => {
         $("#navbar-list > li a").each(function() { $(this).toggleClass("active", $(this).attr('href') == window.location.pathname) })
     })
 
+    // footer
     $(".foot-wrapper").load("/layouts/footer.html")
 
 })
